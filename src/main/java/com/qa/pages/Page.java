d812c69d1f9cbcb81cece0ac048fe9106f3b9d66
Naincy23 <62692152+Naincy23@users.noreply.github.com>
2021-01-24 17:14:44 +0530

    push

--- src/main/java/com/qa/pages/Page.java
+++ src/main/java/com/qa/pages/Page.java
@@ -19,7 +19,7 @@ public abstract class Page {
 	public abstract WebElement getElement(By locator, String elementdata);
 
 	
-	public abstract void waitForElement(By locator);//ggg
+	public abstract void waitForElement(By locator);//ggg//
  
 	
 
